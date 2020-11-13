aeafd3f6c0caf783a7e97e2adb859f167ec57fd9
Borui Tao <borui.tao@mail.mcgill.ca>
2020-11-13 01:09:22 -0500

    fix

--- RMI/Client/run_client_test.sh
+++
@@ -1,3 +0,0 @@
-# Usage: ./run_client.sh [<server_hostname> [<server_rmiobject>]]
-
-java -Djava.security.policy=java.policy -cp javac -cp ../Server/RMIInterface.jar:../Server/RMHashMap.jar:../Server/RMItem.jar:. Client.RMIClientTest $1 $2
